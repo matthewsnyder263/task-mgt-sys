7527475735885dfeb7bccf807525003c4bc27c7f
Matt Snyder <msnyd87@gmail.com>
2023-04-24 18:49:58 -0400

    if this commit was applied, Feature 14 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
+from projects.forms import ProjectForm
 
 
 # Create your views here.
@@ -20,3 +21,20 @@ def show_project(request, id):
         "project_object": project,
     }
     return render(request, "projects/project_detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create_project.html", context)
